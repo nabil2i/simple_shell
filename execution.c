0950e91183abc42782caf4bb7d565e446ea2cd3e
Nabil Affo <nabil.noble01@gmail.com>
2022-05-18 18:03:52 +0000

    Add code files

--- execution.c
+++ execution.c
@@ -1,32 +1,50 @@
 #include "main.h"
 
 /**
- * exe - executes new processes
- * @line
- * Return: Success 0
+ * exe - function to execute new processes
+ * @line: input from user
+ * @ar: array to be handled by execve
+ * @nln: duplicate of line
+ * @arry: Tokens to check
+ * @argv: arguments
+ * @flcnt: number of commands that have failed
+ * Return: 0 for success
  */
-int exe(char **tokens)
+
+int exe(char *line, char **ar, char *nln, char **arry, char **argv, int flcnt)
 {
-	int status;
-	pid_t child_pid;
+	int status, onpath = -1;
+	pid_t child;
+	struct stat *statbuf;
 
-
-	child_pid = fork();
-	if (child_pid == 0)
+	statbuf = malloc(sizeof(struct stat)); /* creates status buffer */
+	if (stat(ar[0], statbuf) == -1) /* checks if filename or path */
 	{
-		if(execve(tokens[0], tokens, NULL) == -1)
+		onpath = findonpath(ar); /* if file name, searchespath for it */
+		if (onpath == 0)
 		{
-			printf("%s: not found\n", tokens[0]);
-			_exit(-1);	
-		
+			free(statbuf); /* if on path go back */
+			return (0);
+		}
+	}
+	child = fork(); /* forks a child process */
+	if (child == 0)
+	{
+		if (execve(ar[0], ar, environ) == -1) /* attempt execute file */
+		{
+			_printf("%s: %d: %s: not found\n", argv[0], flcnt, ar[0]);
+			free(statbuf);
+			myfree(line, ar, nln, arry);
+			_exit(-1); /* terminates the child */
 		}
 	}
 	else
 	{
-		waitpid(-1, &status, 0);
+		while (waitpid(-1, &status, 0) != child) /* waits for child */
+			;
 	}
-	
-	if (status == 0)
+	free(statbuf);
+	if (status == 0) /* checks if execve failed or succeeded */
 		errno = 0;
 	if (status == 512)
 		errno = 2;
@@ -34,3 +52,4 @@ int exe(char **tokens)
 		errno = 127;
 	return (0);
 }
+
--- main.h
+++ main.h
@@ -33,4 +33,48 @@ int _strcmp(char *, char *);
 int _strncmp(char *, char *, unsigned int);
 void exitpls(void);
 void freezeLl(nodep *);
+/* **********************************
+ * **********************************
+ * **********************************
+ */
+/* ENVIRONMENT */
+
+extern char **environ;
+
+/* STRUCTURES*/
+
+/**
+ * struct fmt - types of data and their function
+ * @ltr: the type of data
+ * @type: pointer to function for type
+ */
+typedef struct fmt
+{
+	char *ltr;
+	int (*type)();
+} fmt;
+
+/* DECLARATION OF FUNCTIONS */
+
+void _puts(char *str);
+char *_strdup(char *str);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+int parse(char *line, int num_tokens, char *argv[], int failcount);
+int exe(char *line, char **ar, char *nln, char **arry, char **argv, int flcnt);
+void myfree(char *line, char **ar, char *newline, char **array);
+int _strncmp(char *s1, char *s2, int len);
+int checkbltin(char *line, char **ar, char *newline, char **array);
+int numcount(char *line);
+int searchpath(char *p, char **tokens);
+int findonpath(char **tokens);
+void executepath(char *p, char **tokens);
+int _printf(const char *format, ...);
+int chkfmt(va_list *args, const char *format, int i);
+int prchr(va_list *args);
+int prstr(va_list *args);
+int prdgt(va_list *args);
+void getdigits(int n);
+int _putchar(char c);
+void changedir(char **ar);
 #endif
